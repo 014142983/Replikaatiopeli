8aeadfc8a6ae3f13ac5f1f730f32b801be9271fe
014142983 <leo.kallonen@gmail.com>
2014-01-16 19:09:46 +0200

    gitignore

---
+++ Replikaatiopeli/src/main/java/ohha/replikaatiopeli/App.java
@@ -0,0 +1,13 @@
+package ohha.replikaatiopeli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
